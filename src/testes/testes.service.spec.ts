1f4141b222801cc97b4bf911e5a2bc67406a6491
rafaellevissa <rafaellevi.costa@e-stracta.com>
2024-08-10 13:43:31 -0300

    feat: add test cases

--- src/testes/testes.service.spec.ts
+++ src/testes/testes.service.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TestesService } from './testes.service';
 import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { Testes } from './schemas/testes.schema';
+import { NotFoundException } from '@nestjs/common';
+import { CreateTestisDto } from './dto/create-testis.dto';
 
 describe('TestesService', () => {
   let service: TestesService;
+  let model: Model<Testes>;
+  const mockTestes = [
+    { _id: '1', testValue: 'Teste 1', otherValue: 1 },
+    { _id: '2', testValue: 'Teste 2', otherValue: 2 },
+  ];
 
-  const execMock = jest.fn();
-  const mockTestesRepository = {
-    find: jest.fn().mockReturnValue({ exec: execMock }),
-    findOne: jest.fn(),
-    create: jest.fn(),
-    remove: jest.fn(),
+  const mockTestModel = {
+    find: jest.fn().mockReturnValue({
+      exec: jest.fn().mockResolvedValue(mockTestes),
+    }),
+    findById: jest.fn().mockImplementation((id: string) => ({
+      exec: jest
+        .fn()
+        .mockResolvedValue(
+          mockTestes.find((teste) => teste._id === id) || null,
+        ),
+    })),
+    create: jest.fn().mockImplementation((dto: CreateTestisDto) => {
+      return {
+        ...dto,
+        save: jest.fn().mockResolvedValue({ _id: '3', ...dto }),
+      };
+    }),
+    findByIdAndUpdate: jest.fn().mockImplementation((id, dto) => ({
+      exec: jest.fn().mockResolvedValue({ _id: id, ...dto }),
+    })),
+    deleteOne: jest.fn().mockResolvedValue({ deletedCount: 1 }),
   };
+
+  jest.mock('mongoose', () => ({
+    Model: jest.fn().mockImplementation(() => mockTestModel),
+  }));
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,29 +47,62 @@ describe('TestesService', () => {
         TestesService,
         {
           provide: getModelToken(Testes.name),
-          useValue: mockTestesRepository,
+          useValue: mockTestModel,
         },
       ],
     }).compile();
 
     service = module.get<TestesService>(TestesService);
+    model = module.get<Model<Testes>>(getModelToken(Testes.name));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should create a new test object', async () => {
+    const newTest = { testValue: 'Teste 3', otherValue: 3 };
+    const createdTest = { _id: '3', ...newTest };
+
+    jest.spyOn(model, 'create').mockImplementation((dto: any) => ({
+      ...dto,
+      save: jest.fn().mockResolvedValue(createdTest),
+    }));
+
+    const result = await service.create(newTest);
+    expect(result).toEqual(createdTest);
   });
 
-  describe('findAll', () => {
-    it('should return all tests', async () => {
-      const result: Testes[] = [
-        {
-          testValue: 'Teste',
-          otherValue: 1,
-        } as Testes,
-      ];
-      execMock.mockResolvedValueOnce(result);
+  it('should return all tests objects', async () => {
+    const result = await service.findAll();
+    expect(result).toEqual(mockTestes);
+  });
 
-      expect(await service.findAll()).toBe(result);
-    });
+  it('should return one test object by ID', async () => {
+    const result = await service.findOne('1');
+    expect(result).toEqual(mockTestes[0]);
+  });
+
+  it('should throw an error when a test object is not found', async () => {
+    jest.spyOn(model, 'findById').mockReturnValue({
+      exec: jest.fn().mockResolvedValue(null),
+    } as any);
+
+    await expect(service.findOne('3')).rejects.toThrow(NotFoundException);
+  });
+
+  it('should update an existing test object', async () => {
+    const updateTest = { testValue: 'Teste Updated', otherValue: 1 };
+    const updatedTest = { _id: '1', ...updateTest };
+    jest
+      .spyOn(model, 'findByIdAndUpdate')
+      .mockResolvedValue(updatedTest as any);
+
+    const result = await service.update('1', updateTest);
+    expect(result).toEqual(updatedTest);
+  });
+
+  it('should delete a test object by ID', async () => {
+    const mockDeleteResult = { deletedCount: 1 };
+    jest.spyOn(model, 'deleteOne').mockResolvedValue(mockDeleteResult as any);
+
+    const result = await service.remove('1');
+    expect(result.deletedCount).toEqual(1);
   });
 });
--- src/testes/testes.service.ts
+++ src/testes/testes.service.ts
@@ -1,14 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Testes } from './schemas/testes.schema';
 import { CreateTestisDto } from './dto/create-testis.dto';
 import { UpdateTestisDto } from './dto/update-testis.dto';
-import { InjectModel } from '@nestjs/mongoose';
-import { Testes } from './schemas/testes.schema';
-import { Model } from 'mongoose';
-import { DeleteResult } from 'typeorm/driver/mongodb/typings';
 
 @Injectable()
 export class TestesService {
-  constructor(@InjectModel(Testes.name) private testeModel: Model<Testes>) {}
+  constructor(
+    @InjectModel(Testes.name) private readonly testeModel: Model<Testes>,
+  ) {}
 
   async create(createTestisDto: CreateTestisDto): Promise<Testes> {
     const createTeste = new this.testeModel(createTestisDto);
@@ -20,20 +21,28 @@ export class TestesService {
   }
 
   async findOne(id: string): Promise<Testes> {
-    return this.testeModel.findById(id);
+    const teste = await this.testeModel.findById(id).exec();
+    if (!teste) {
+      throw new NotFoundException(`Test with ID ${id} not found`);
+    }
+    return teste;
   }
 
-  async update(id: string, updateTestisDto: UpdateTestisDto) {
-    return await this.testeModel.updateOne(
-      { _id: id },
-      {
-        testValue: updateTestisDto?.testValue,
-        otherValue: updateTestisDto?.otherValue,
-      },
-    );
+  async update(id: string, updateTestisDto: UpdateTestisDto): Promise<Testes> {
+    const updatedTeste = await this.testeModel
+      .findByIdAndUpdate(id, updateTestisDto, { new: true })
+      .exec();
+    if (!updatedTeste) {
+      throw new NotFoundException(`Test with ID ${id} not found`);
+    }
+    return updatedTeste;
   }
 
-  async remove(id: string): Promise<DeleteResult> {
-    return this.testeModel.deleteOne({ _id: id });
+  async remove(id: string): Promise<{ deletedCount?: number }> {
+    const result = await this.testeModel.deleteOne({ _id: id }).exec();
+    if (result.deletedCount === 0) {
+      throw new NotFoundException(`Test with ID ${id} not found`);
+    }
+    return result;
   }
 }
